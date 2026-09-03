68f326c6b258d86b594b1dd2b66a177b41d0d56c
2601964 <pegnk3198@gmail.com>
2026-09-03 16:59:48 +0900

    111

--- src/main/java/me/scpark/TestController.java
+++ src/main/java/me/scpark/TestController.java
@@ -1,5 +1,6 @@
 package me.scpark;
 
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -10,5 +11,16 @@ public class TestController {
     }
     @GetMapping("/test")
     public String test() {
+        return "hello? 'GET http://localhost:8080/test' request";}
+
+    @PostMapping ("/test")
+    public String posttest() {
+        return "hello? 'POST http://localhost:8080/post' request";}
+    @PutMapping ("/test")
+    public String puttest() {
+        return "hello? 'PUT http://localhost:8080/test' request";}
+    @DeleteMapping ("/test")
+    public String delettest() {
+        return "hello? 'DELETE http://localhost:8080/test' request";
     }
 }
